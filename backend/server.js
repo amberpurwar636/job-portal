18cf7c2c80d907b232be313677794d9a8bd47f8b
amberpurwar636 <149404087+amberpurwar636@users.noreply.github.com>
2025-06-26 14:13:10 +0530

    updated files

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
+import express from "express";
 import app from "./app.js";
 import cloudinary from "cloudinary";
+import path from "path";
 
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, // CLOUDINARY_CLIENT_NAME
@@ -7,6 +9,13 @@ cloudinary.v2.config({
   api_secret: process.env.CLOUDINARY_API_SECRET, // CLOUDINARY_CLIENT_SECRET
 });
 
+const _dirname=path.resolve();
+app.use(express.static(path.join(_dirname,"/frontend/dist")));
+app.get('*',(_,res)=>{
+  res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"));
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Server running at port ${process.env.PORT}`);
 });
+ 
